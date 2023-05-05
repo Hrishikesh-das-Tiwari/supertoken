f82c85a134d7c92eede04f9147fc7cfd1cab9687
Hrishikesh A. Tiwari <hrishikeshtiwari@Hrishikeshs-MacBook-Air.local>
2023-05-05 20:20:50 +0530

    initial

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -2,11 +2,11 @@ const port = process.env.APP_PORT || 3000;
 
 const apiBasePath = "/api/auth/";
 
-export const websiteDomain = process.env.APP_URL || process.env.NEXT_PUBLIC_APP_URL || `http://localhost:${port}`;
+export const websiteDomain = "https://supertoken-silk.vercel.app/";
 
 export const appInfo = {
-    appName: "SuperTokens Demo App",
-    websiteDomain,
-    apiDomain: websiteDomain,
-    apiBasePath,
+  appName: "SuperTokens Demo App",
+  websiteDomain,
+  apiDomain: websiteDomain,
+  apiBasePath,
 };
